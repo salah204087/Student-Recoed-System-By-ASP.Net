8da78a355323b97784121bf29f199f01d810e8df
salah204087 <salah204087@gmail.com>
2023-06-19 16:53:26 +0300

    Add Analysis Controller to show the number of university,factuly,school and student in system and add the analysis button in navbar to show the page.

---
+++ StudentRecordSystem/Controllers/AnalysisController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentRecordSystem.Data;
+
+namespace StudentRecordSystem.Controllers
+{
+    public class AnalysisController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AnalysisController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var Analysis = new object[4];
+            Analysis[0] = _context.Students.Count();
+            Analysis[1]=_context.Universities.Count();
+            Analysis[2]=_context.Faculties.Count();
+            Analysis[3] = _context.Schools.Count();
+            return View(Analysis);
+        }
+    }
+}
---
+++ StudentRecordSystem/wwwroot/js/analysis.js
@@ -0,0 +1,12 @@
+﻿// Reference: http://www.i-visionblog.com/2014/11/jquery-animated-number-counter-from-zero-to-value-jquery-animation.html 
+$('.count').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
